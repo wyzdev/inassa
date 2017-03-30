3d3261887544bc8963e01eede5dd11ec81fdcb11
Hollyn Derisse <hollynderisse93@gmail.com>
2017-03-30 16:51:03 -0400

    login interface done

--- app/src/main/java/com/inassa/inassa/constants/Constants.java
+++ app/src/main/java/com/inassa/inassa/constants/Constants.java
@@ -5,9 +5,10 @@ package com.inassa.inassa.constants;
  */
 
 public class Constants {
-    public static final String SERVER = "http://192.168.0.124:8765/";
+    public static final String SERVER = "http://192.168.0.129:8765/";
     //public static final String SERVER = "http://192.168.15.77:8765/";
 
+    public static final String LOGIN_ADDRESS = SERVER + "users/login";
     public static final String ADD_ADDRESS = SERVER + "logs/add.json";
 
     public static final String KEY_GLOBAL_NUMBER = "global_number";
